e65ccad205ab5814fd272ef2a2a8c6452ec88cd7
Skye Soss <skyler.soss@gmail.com>
2024-10-08 17:32:19 -0500

    Update smlnj.nix

--- NixOS/pkgs/smlnj.nix
+++ NixOS/pkgs/smlnj.nix
@@ -102,9 +102,9 @@ in
       patchPhase = ''
         sed -i '/^PATH=/d' config/_arch-n-opsys base/runtime/config/gen-posix-names.sh
         echo SRCARCHIVEURL="file:/$TMP" > config/srcarchiveurl
-      '' + (if stdenv.isDarwin then ''
-        sed -i 's:INCLFILE=.*$:INCLFILE=${stdenv.cc.libc}/include/unistd.h:' base/runtime/config/gen-posix-names.sh
-      '' else "");
+      '' + lib.optionalString stdenv.isDarwin ''
+        sed -i '/^INCLFILE=/c INCLFILE=${stdenv.cc.libc}/include/unistd.h' base/runtime/config/gen-posix-names.sh
+      '';
 
       unpackPhase = ''
         for s in $sources; do
