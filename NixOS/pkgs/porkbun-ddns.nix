c5e88b0de8742f211edda05f60b58573620641ad
ssoss <skyler.soss@gmail.com>
2024-10-19 13:42:47 -0500

    porkbun-ddns to 1.1.8

--- NixOS/pkgs/porkbun-ddns.nix
+++ NixOS/pkgs/porkbun-ddns.nix
@@ -7,12 +7,12 @@
 }:
 python3Packages.buildPythonApplication {
   pname = "porkbun-ddns";
-  version = "1.1.4-alpha";
+  version = "1.1.8";
   src = fetchFromGitHub {
     owner = "mietzen";
     repo = "porkbun-ddns";
-    rev = "92f1434a88071be0ab6b99845e5bff17574b0a3c";
-    hash = "sha256-awkEx0wDj/eHUSCRRJSytzMXRWtMewYtGcUT2TmguCY=";
+    rev = "558edfee4a8ae3f0e8052cf6ed2a0afdf8ce0084";
+    hash = "sha256-HWHglpz/rRoLP4opMsraZeQ0gPA8MaQTmNUuodZ+eyY=";
   };
   build-system = [python3Packages.setuptools];
   dependencies = [python3Packages.xdg-base-dirs];
