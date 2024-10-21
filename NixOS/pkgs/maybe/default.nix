a96111a5be28b6fc18eb8b32ac0a54294fdb7aea
ssoss <skyler.soss@gmail.com>
2024-10-21 13:48:13 -0500

    First attempt at adding maybe, currently doesn't compile

---
+++ NixOS/pkgs/maybe/default.nix
@@ -0,0 +1,34 @@
+{
+  bundlerApp,
+  bundlerUpdateScript,
+  fetchFromGitHub,
+  git,
+  lib,
+  postgresql,
+  vips,
+  ...
+}:
+let
+  source = fetchFromGitHub {
+    owner = "maybe-finance";
+    repo = "maybe";
+    rev = "728b10d08e017338876ceb945fe472430d66739a";
+    hash = "sha256-IV5MJNgBVGpT3icZRF9XLlwZAlYERRjOTOeVLYsiHIY=";
+  };
+in
+bundlerApp {
+  pname = "maybe";
+  gemdir = ./.;
+  buildInputs = [vips postgresql];
+  nativeBuildInputs = [git];
+  exes = [];
+  passthru.updateScript = bundlerUpdateScript "maybe";
+  meta = {
+    description = "Maybe: The OS for your personal finances";
+    homepage = "https://maybe.co";
+    license = lib.licenses.agpl3Plus;
+    maintainers = [];
+    platforms = lib.platforms.unix;
+    # mainProgram = "";
+  };
+}
