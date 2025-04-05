d4ce5fb621a090efeafacd209d6670eb083ec562
ssoss <skyler.soss@gmail.com>
2025-04-05 11:15:17 -0500

    Test my fork of porkbun DDNS

--- NixOS/pkgs/porkbun-ddns.nix
+++ NixOS/pkgs/porkbun-ddns.nix
@@ -9,10 +9,11 @@ python3Packages.buildPythonApplication {
   pname = "porkbun-ddns";
   version = "1.1.8";
   src = fetchFromGitHub {
-    owner = "mietzen";
+    # owner = "mietzen";
+    owner = "Skyb0rg007";
     repo = "porkbun-ddns";
-    rev = "531d239234b76a41425fca51a2c3ed0244a2ae68";
-    hash = "sha256-p3DPVS/c0qQr2mCL7zS7UV0jr+aYum5mSgDzaNAjy2s=";
+    rev = "c7cfab36c3cf9a013ec1418d904ff5117290d017";
+    hash = "sha256-9ofhjN3gDilRmHLt5sIbyip85mWWCEXnKLYmO/skuik=";
   };
   build-system = [python3Packages.setuptools];
   dependencies = [python3Packages.xdg-base-dirs];
