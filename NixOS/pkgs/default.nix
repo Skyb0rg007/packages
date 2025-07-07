0357483362ca059f244c98286dfa6dc7faa7ee8f
ssoss <skyler.soss@gmail.com>
2025-07-07 14:36:56 -0500

    Remove mlton and smlnj

--- NixOS/pkgs/default.nix
+++ NixOS/pkgs/default.nix
@@ -2,11 +2,11 @@
   anemoi = pkgs.callPackage ./anemoi.nix {};
   ascsaver = pkgs.callPackage ./ascsaver.nix {};
   # maybe = pkgs.callPackage ./maybe {};
-  mlton-dev = pkgs.callPackage ./mlton.nix {};
+  # mlton-dev = pkgs.callPackage ./mlton.nix {};
   porkbun-ddns = pkgs.callPackage ./porkbun-ddns.nix {};
   # receipt-wrangler = pkgs.callPackage ./receipt-wrangler.nix {};
   # receipt-wrangler-desktop = pkgs.callPackage ./receipt-wrangler-desktop.nix {};
-  smlnj = pkgs.callPackage ./smlnj.nix {};
+  # smlnj = pkgs.callPackage ./smlnj.nix {};
   smlnj-dev = pkgs.callPackage ./smlnj-dev.nix {};
   tmux-notify = pkgs.callPackage ./tmux-notify.nix {};
   recutils = pkgs.callPackage ./recutils.nix {};
