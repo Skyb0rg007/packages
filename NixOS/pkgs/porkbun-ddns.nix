4b5031a3d5803eb7eee949b7685a9105fcbf9eb8
ssoss <skyler.soss@gmail.com>
2025-08-14 17:40:17 -0500

    porkbun-ddns: Update to 1.1.14, added pytest check

--- NixOS/pkgs/porkbun-ddns.nix
+++ NixOS/pkgs/porkbun-ddns.nix
@@ -1,25 +1,28 @@
 {
   lib,
   pkgs,
-  fetchFromGitHub,
+  fetchurl,
   python3Packages,
   ...
-}:
-python3Packages.buildPythonApplication {
-  pname = "porkbun-ddns";
-  version = "1.1.8";
-  src = fetchFromGitHub {
-    # owner = "mietzen";
-    owner = "Skyb0rg007";
-    repo = "porkbun-ddns";
-    rev = "c7cfab36c3cf9a013ec1418d904ff5117290d017";
-    hash = "sha256-9ofhjN3gDilRmHLt5sIbyip85mWWCEXnKLYmO/skuik=";
-  };
-  build-system = [python3Packages.setuptools];
-  dependencies = [python3Packages.xdg-base-dirs];
-  meta = {
-    description = "An unofficial DDNS-Client for Porkbun Domains";
-    homepage = "https://github.com/mietzen/porkbun-ddns";
-    license = lib.licenses.mit;
-  };
-}
+}: let
+  version = "1.1.14";
+in
+  python3Packages.buildPythonApplication {
+    pname = "porkbun-ddns";
+    inherit version;
+    src = fetchurl {
+      url = "https://github.com/mietzen/porkbun-ddns/archive/refs/tags/v${version}.tar.gz";
+      hash = "sha256-DFlGUA7+lh0JJBTEDKVPchfXsVK5C2wCMh6bMnqrZn8=";
+    };
+    pyproject = true;
+    build-system = [python3Packages.setuptools];
+    dependencies = [python3Packages.xdg-base-dirs];
+    nativeCheckInputs = [python3Packages.pytestCheckHook];
+    enabledTestPaths = ["porkbun_ddns/test"];
+    meta = {
+      description = "An unofficial DDNS-Client for Porkbun Domains";
+      homepage = "https://github.com/mietzen/porkbun-ddns";
+      license = lib.licenses.mit;
+      mainProgram = "porkbun-ddns";
+    };
+  }
