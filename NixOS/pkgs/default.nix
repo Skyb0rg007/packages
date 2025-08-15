26603e5b85bce2ec366dcb5e80c3824a0f915be8
ssoss <skyler.soss@gmail.com>
2025-08-15 17:01:46 -0500

    Removed abandoned packaging efforts

--- NixOS/pkgs/default.nix
+++ NixOS/pkgs/default.nix
@@ -1,12 +1,8 @@
 {pkgs ? import <nixpkgs> {}, ...}: {
   anemoi = pkgs.callPackage ./anemoi.nix {};
   ascsaver = pkgs.callPackage ./ascsaver.nix {};
-  # maybe = pkgs.callPackage ./maybe {};
   mlton = pkgs.callPackage ./mlton.nix {};
   porkbun-ddns = pkgs.callPackage ./porkbun-ddns.nix {};
-  # receipt-wrangler = pkgs.callPackage ./receipt-wrangler.nix {};
-  # receipt-wrangler-desktop = pkgs.callPackage ./receipt-wrangler-desktop.nix {};
-  # smlnj = pkgs.callPackage ./smlnj.nix {};
   smlnj-dev = pkgs.callPackage ./smlnj-dev.nix {};
   tmux-notify = pkgs.callPackage ./tmux-notify.nix {};
   recutils = pkgs.callPackage ./recutils.nix {};
