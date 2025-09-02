a6a136428e5787708d5094aa5591e1eff291d675
ssoss <skyler.soss@gmail.com>
2025-09-02 12:00:29 -0500

    browsertrix-crawler: Add ./bin directory wrappers

--- NixOS/pkgs/browsertrix-crawler.nix
+++ NixOS/pkgs/browsertrix-crawler.nix
@@ -1,43 +1,33 @@
 {
   fetchFromGitHub,
-  runCommand,
   fetchYarnDeps,
   fetchurl,
-  stevenblack-blocklist,
-  lib,
+  fetchzip,
+  google-chrome,
   jq,
+  lib,
   makeWrapper,
   node-gyp,
   nodejs,
   pkg-config,
   python3,
+  redis,
+  runCommand,
   stdenv,
+  socat,
+  stevenblack-blocklist,
   vips,
+  x11vnc,
+  xorg,
   yarnBuildHook,
   yarnConfigHook,
-  yarnInstallHook,
 }: let
   version = "1.7.0";
   rwp_version = "2.3.15";
-  rwp_ui = fetchurl {
-    url = "https://cdn.jsdelivr.net/npm/replaywebpage@${rwp_version}/ui.js";
-    hash = "sha256-uv3sAk2sXoganAh9vDtF2lcfKm4UBvIhQgkOx5ruXo8=";
+  rwp = fetchzip {
+    url = "https://registry.npmjs.org/replaywebpage/-/replaywebpage-${rwp_version}.tgz";
+    hash = "sha256-C70E5RZ7J6wtp67pCbUeA4nRpABFPlFO0ATYfK8AfJc=";
   };
-  rwp_sw = fetchurl {
-    url = "https://cdn.jsdelivr.net/npm/replaywebpage@${rwp_version}/sw.js";
-    hash = "sha256-WphWRLYy3eIxle9BL3q4x15hXJlP8V196Z4LjJhfov4=";
-  };
-  rwp_adblock = fetchurl {
-    url = "https://cdn.jsdelivr.net/npm/replaywebpage@${rwp_version}/adblock/adblock.gz";
-    hash = "sha256-yOQkXREun8MCgr/1Lav7aZpe6Ca8F6d2wNimj9vimIw=";
-  };
-  # blocklist = fetchurl {
-  #   url = "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts";
-  #   hash = "sha256-/jCtv+KJN6jRYAG03FPQQN9AF3YKrTRKd72lilZMgNE=";
-  # };
-  ad_blocklist = runCommand "ad-hosts.json" {} ''
-    grep '^0\.0\.0\.0 ' < ${stevenblack-blocklist}/hosts | awk '{ print $2; }' | grep -v '0\.0\.0\.0' | ${lib.getExe jq} --raw-input --slurp 'split("\n")' > $out
-  '';
 in
   stdenv.mkDerivation (finalAttrs: {
     pname = "browsertrix-crawler";
@@ -63,7 +53,6 @@ in
     nativeBuildInputs = [
       yarnConfigHook
       yarnBuildHook
-      # yarnInstallHook
       nodejs
       node-gyp
       makeWrapper
@@ -86,26 +75,40 @@ in
     '';
 
     installPhase = ''
-      mkdir -pv $out/lib/node_modules/browsertrix-crawler/html/rwp
+      mkdir -pv $out/{bin,lib/node_modules/browsertrix-crawler/html/rwp}
+
       cp -rv ./* $out/lib/node_modules/browsertrix-crawler/
-      chmod +x $out/lib/node_modules/browsertrix-crawler/dist/main.js
 
-      ln -s ${rwp_ui} $out/lib/node_modules/browsertrix-crawler/html/rwp/ui.js
-      ln -s ${rwp_sw} $out/lib/node_modules/browsertrix-crawler/html/rwp/sw.js
-      ln -s ${rwp_adblock} $out/lib/node_modules/browsertrix-crawler/html/rwp/adblock.gz
-      ln -s ${ad_blocklist} $out/lib/node_modules/browsertrix-crawler/ad-hosts.json
+      cp ${rwp}/ui.js $out/lib/node_modules/browsertrix-crawler/html/rwp/ui.js
+      cp ${rwp}/sw.js $out/lib/node_modules/browsertrix-crawler/html/rwp/sw.js
+      cp ${rwp}/adblock/adblock.gz $out/lib/node_modules/browsertrix-crawler/html/rwp/adblock.gz
+      grep '^0\.0\.0\.0 ' ${stevenblack-blocklist}/hosts \
+        | awk '{ print $2; }' \
+        | grep -v '0\.0\.0\.0\( \|$\)' \
+        | ${lib.getExe jq} --raw-input --slurp 'split("\n")' \
+          > $out/lib/node_modules/browsertrix-crawler/ad-hosts.json
+
+      makeWrapper ${lib.getExe nodejs} $out/bin/browsertrix-crawl \
+        --add-flag --experimental-global-webcrypto \
+        --add-flag $out/lib/node_modules/browsertrix-crawler/dist/main.js \
+        --prefix PATH : ${lib.makeBinPath [redis xorg.xvfb]} \
+        --set BROWSER_BIN ${lib.getExe google-chrome} \
+        --set BROWSER_VERSION ${google-chrome.version} \
+        --set-default GEOMETRY "1360x1020x16" \
+        --set-default VNC_PASS "vncpassw0rd!" \
+        --set-default DETACHED_CHILD_PROC "1"
+
+      ln -s $out/bin/browsertrix-crawl $out/bin/browsertrix-qa
+
+      makeWrapper ${lib.getExe nodejs} $out/bin/browsertrix-create-login-profile \
+        --add-flag $out/lib/node_modules/browsertrix-crawler/dist/create-login-profile.js \
+        --prefix PATH : ${lib.makeBinPath [x11vnc xorg.xvfb socat]} \
+        --set BROWSER_BIN ${lib.getExe google-chrome} \
+        --set BROWSER_VERSION ${google-chrome.version} \
+        --set-default GEOMETRY "1360x1020x16" \
+        --set-default VNC_PASS "vncpassw0rd!" \
+        --set-default DETACHED_CHILD_PROC "1"
     '';
-
-    # installPhase = ''
-    #   # mkdir -pv $out/{bin,share/browsertrix-crawler}
-    #   # cp -rv ./dist/* $out/share/browsertrix-crawler
-
-    #   mkdir -p $out
-    #   cp -r . $out/
-
-    #   # makeWrapper ${lib.getExe nodejs} $out/bin/browsertrix-crawl \
-    #   #   --arg $out/share/browsertrix-crawler/main.js
-    # '';
 
     meta = {
       description = "A simplified browser-based crawling system";
