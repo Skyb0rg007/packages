00b1ddd1c51ae140b96579dd64555e1201902384
Skye Soss <skye@soss.website>
2025-12-25 11:14:37 -0500

    radicle-ci-ambient: use the crate as the source

--- NixOS/pkgs/radicle-ci-ambient.nix
+++ NixOS/pkgs/radicle-ci-ambient.nix
@@ -1,15 +1,15 @@
 {
   lib,
-  fetchgit,
+  fetchCrate,
   rustPlatform,
 }:
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "radicle-ci-ambient";
   version = "0.12.0";
 
-  src = fetchgit {
-    url = "https://radicle.liw.fi/z35CgFVYCKpqqDtJMzk8dyE6dViS6.git";
-    hash = "sha256-3WIAhE9vDgC14V9PJWUUriXFxzDEf1x0/05k4aq/TM0=";
+  src = fetchCrate {
+    inherit (finalAttrs) pname version;
+    hash = "sha256-sLjSE4DR1Fj+vIu/Oouge93BqdGkA3jf+OK8yANtTNw=";
   };
 
   cargoHash = "sha256-QR8pODzNGRsrfoEh2mlWwEmdiDU2ZulRs4UHbOX2ArU=";
