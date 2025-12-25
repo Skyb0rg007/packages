1cf0caa930f09edf85f64034b5af01e95b97593c
Skye Soss <skye@soss.website>
2025-12-25 17:23:48 -0500

    Remove mlton and smlnj

--- NixOS/pkgs/default.nix
+++ NixOS/pkgs/default.nix
@@ -1,9 +1,7 @@
 {pkgs ? import <nixpkgs> {}, ...}: {
   anemoi = pkgs.callPackage ./anemoi.nix {};
   ascsaver = pkgs.callPackage ./ascsaver.nix {};
-  mlton = pkgs.callPackage ./mlton.nix {};
   porkbun-ddns = pkgs.callPackage ./porkbun-ddns.nix {};
-  smlnj-dev = pkgs.callPackage ./smlnj-dev.nix {};
   tmux-notify = pkgs.callPackage ./tmux-notify.nix {};
   romm = pkgs.callPackage ./romm.nix {};
 
