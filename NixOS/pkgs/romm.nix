440ea606d3536044b69accdcdaea16bb5c8955ca
Skye Soss <skye@soss.website>
2025-12-25 18:52:59 -0500

    romm 4.4.1

--- NixOS/pkgs/romm.nix
+++ NixOS/pkgs/romm.nix
@@ -1,32 +1,34 @@
 {
-  stdenv,
+  buildNpmPackage,
   config,
   fetchFromGitHub,
   fetchPypi,
-  buildNpmPackage,
   python3Packages,
-  ...
+  stdenv,
 }: let
   inherit (python3Packages) buildPythonPackage;
-  version = "4.3.2";
+  version = "4.4.1";
   src = fetchFromGitHub {
     owner = "rommapp";
     repo = "romm";
     rev = "refs/tags/${version}";
-    hash = "sha256-Gc3QVggLVIA5nlSsFhS4w8anBqGnKVG7VUILgMMIu8U=";
+    hash = "sha256-0xcGd0z4uNCFbqo6xiRqde83g2Ns1NZBB6CzfqZRjVU=";
   };
+
   frontend = buildNpmPackage {
     pname = "romm-frontend";
     inherit version;
     src = "${src}/frontend";
-    npmDepsHash = "sha256-d5BQsbJsLP2tKAEKK42ai7RprVbUNZ0zTAVq+4rR0J4=";
+    npmDepsHash = "sha256-ytbH58i1D63Z73tG4WLSDsLjscjSRjWg0yqOrtXhluo=";
   };
+
   backend = buildPythonPackage {
     pname = "romm-backend";
     inherit src version;
     pyproject = true;
     env.HATCHLING_ALLOW_INVALID_DEPENDENCIES = "1";
     postPatch = ''
+      rm -rf docker frontend
       substituteInPlace pyproject.toml \
         --replace-fail "joserfc ~= 1.3.4" "joserfc >= 1.2.2" \
         --replace-fail "opentelemetry-distro ~= 0.56" "opentelemetry-distro >= 0.55b0" \
@@ -38,11 +40,17 @@
         --replace-fail "opentelemetry-instrumentation-sqlalchemy ~= 0.56" "opentelemetry-instrumentation-sqlalchemy >= 0.55b0" \
         --replace-fail "pillow ~= 10.3" "pillow >= 10.3" \
         --replace-fail "pydash ~= 7.0" "pydash >= 7.0" \
+        --replace-fail "fastapi[standard-no-fastapi-cloud-cli] ~= 0.121.1" "fastapi[standard-no-fastapi-cloud-cli] >= 0.116.0" \
+        --replace-fail "starlette ~= 0.49" "starlette >= 0.47.2" \
         --replace-fail "watchfiles ~= 1.1" "watchfiles >= 1.0.5"
-      printf '[build-system]\nrequires=["hatchling"]\nbuild-backend="hatchling.build"\n' >> pyproject.toml
-      printf '[tool.hatch.metadata]\nallow-direct-references=true\n' >> pyproject.toml
-      printf '[tool.hatch.build.targets.wheel]\npackages=["backend"]\n' >> pyproject.toml
+      printf '[project.scripts]\nromm="backend.main:main"\n' >> pyproject.toml
     '';
+
+    postInstall = ''
+      mkdir -p $out/share/romm
+      cp -r ${frontend}/lib/node_modules/romm/dist $out/share/romm/frontend
+    '';
+
     build-system = [
       python3Packages.hatchling
     ];
@@ -89,102 +97,19 @@
       starlette-csrf
       streaming-form-data
       strsimpy
-      strsimpy
       types-passlib
       zipfile-inflate64
     ];
   };
 
-  # opentelemetry-api = buildPythonPackage rec {
-  #   pname = "opentelemetry-api";
-  #   version = "1.35.0";
-  #   pyproject = true;
-  #   src = fetchPypi {
-  #     pname = "opentelemetry_api";
-  #     inherit version;
-  #     hash = "sha256-oRG5Wbz6W019/8L71qJBqnLdeN2OebWxZivaiWxdL/4=";
-  #   };
-  #   build-system = [python3Packages.hatchling];
-  #   dependencies = [
-  #     python3Packages.importlib-metadata
-  #     python3Packages.typing-extensions
-  #   ];
-  # };
-
-  # opentelemetry-semantic-conventions = buildPythonPackage rec {
-  #   pname = "opentelemetry-semantic-conventions";
-  #   version = "0.56b0";
-  #   pyproject = true;
-  #   src = fetchPypi {
-  #     pname = "opentelemetry_semantic_conventions";
-  #     inherit version;
-  #     hash = "sha256-wRTC6syP9tOQjLMoyBHq9k5taGI4QL6SJNyCnE/Wwuo=";
-  #   };
-  #   build-system = [python3Packages.hatchling];
-  #   dependencies = [
-  #     opentelemetry-api
-  #   ];
-  # };
-
-  # opentelemetry-instrumentation = buildPythonPackage rec {
-  #   pname = "opentelemetry-instrumentation";
-  #   version = "0.56b0";
-  #   pyproject = true;
-  #   src = fetchPypi {
-  #     pname = "opentelemetry_instrumentation";
-  #     inherit version;
-  #     hash = "sha256-0tuzAhGIyg7IxWBjSe6aKRkjlifoNB1NN/HSHsMpHRE=";
-  #   };
-  #   build-system = [python3Packages.hatchling];
-  #   dependencies = [
-  #     opentelemetry-api
-  #     opentelemetry-semantic-conventions
-  #     python3Packages.wrapt
-  #   ];
-  # };
-
-  # opentelemetry-sdk = buildPythonPackage rec {
-  #   pname = "opentelemetry-sdk";
-  #   version = "1.35.0";
-  #   pyproject = true;
-  #   src = fetchPypi {
-  #     pname = "opentelemetry_sdk";
-  #     inherit version;
-  #     hash = "sha256-KkALQVq2iqpvBOimqfZVKQj7MJCuL/eNauDFl6xYGVQ=";
-  #   };
-  #   build-system = [python3Packages.hatchling];
-  #   dependencies = [
-  #     opentelemetry-api
-  #     opentelemetry-semantic-conventions
-  #     python3Packages.typing-extensions
-  #   ];
-  # };
-
-  # opentelemetry-distro = buildPythonPackage rec {
-  #   pname = "opentelemetry-distro";
-  #   version = "0.56b0";
-  #   pyproject = true;
-  #   src = fetchPypi {
-  #     pname = "opentelemetry_distro";
-  #     inherit version;
-  #     hash = "sha256-h7guLFOk1he5+qp5YDlfc+0Vi/6+x5CSOrrAeWl0/i0=";
-  #   };
-  #   build-system = [python3Packages.hatchling];
-  #   dependencies = [
-  #     opentelemetry-api
-  #     opentelemetry-sdk
-  #     opentelemetry-instrumentation
-  #   ];
-  # };
-
   fastapi-pagination = buildPythonPackage rec {
     pname = "fastapi-pagination";
-    version = "0.14.3";
+    version = "0.15.4";
     pyproject = true;
     src = fetchPypi {
       pname = "fastapi_pagination";
       inherit version;
-      hash = "sha256-vo6B4hI1wHWMvdLw5ZfGW8uCqFBi4rmalHRBjSMAZ5E=";
+      hash = "sha256-vpvgEYE5m+ClS9vZyZG3DtbN46pS1UP/2Sr3YgLuEBM=";
     };
     build-system = [python3Packages.hatchling];
     dependencies = [
