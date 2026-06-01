42bd9ddd06a6708ff0fd0691c1d19fc38401ce49
Skye Soss <skye@soss.website>
2026-06-01 15:21:09 -0500

    Remove ArchiveBox

--- NixOS/default.nix
+++ NixOS/default.nix
@@ -28,44 +28,6 @@ rec {
   vmdb2 = pkgs.callPackage ./pkgs/vmdb2.nix { };
 
   anemoi = pkgs.callPackage ./pkgs/anemoi.nix { };
-  # archivebox = pkgs.callPackage ./pkgs/archivebox.nix {
-  #   python3Packages =
-  #     (pkgs.python314Packages.python.override {
-  #       packageOverrides = self: super: {
-  #         django = self.django_6;
-  #         django_6 = self.callPackage ./pkgs/pythonPackages/django_6.nix { };
-  #         base32-crockford = self.callPackage ./pkgs/pythonPackages/base32-crockford.nix { };
-  #         python-statemachine = self.callPackage ./pkgs/pythonPackages/python-statemachine.nix { };
-  #         sonic-client = self.callPackage ./pkgs/pythonPackages/sonic-client.nix { };
-  #         abx-pkg = self.callPackage ./pkgs/pythonPackages/abx-pkg.nix { };
-  #         django-object-actions = self.callPackage ./pkgs/pythonPackages/django-object-actions.nix { };
-  #         django-settings-holder = self.callPackage ./pkgs/pythonPackages/django-settings-holder.nix { };
-  #         django-admin-data-views = self.callPackage ./pkgs/pythonPackages/django-admin-data-views.nix { };
-  #         django-signal-webhooks = self.callPackage ./pkgs/pythonPackages/django-signal-webhooks.nix { };
-  #         django-extensions = super.django-extensions.overrideAttrs {
-  #           dontUsePytestCheck = true;
-  #         };
-  #         txaio = super.txaio.overrideAttrs (prevAttrs: {
-  #           disabledTests = prevAttrs.disabledTests ++ [
-  #             "test_is_called"
-  #             "test_is_future_generic"
-  #             "test_gather_no_consume"
-  #             "test_gather_two"
-  #             "test_create_result"
-  #             "test_create_error"
-  #           ];
-  #         });
-  #         daphne = super.daphne.overrideAttrs (prevAttrs: {
-  #           doCheck = false;
-  #           dontUsePytestCheck = true;
-  #         });
-  #         autobahn = super.autobahn.overrideAttrs (prevAttrs: {
-  #           doCheck = false;
-  #           dontUsePytestCheck = true;
-  #         });
-  #       };
-  #     }).pkgs;
-  # };
   ascsaver = pkgs.callPackage ./pkgs/ascsaver.nix { };
   mlkit = pkgs.callPackage ./pkgs/mlkit.nix { };
   barry = pkgs.callPackage ./pkgs/barry.nix { inherit mlkit; };
