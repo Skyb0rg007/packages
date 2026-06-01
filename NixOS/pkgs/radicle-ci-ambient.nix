f65146ea5d90b8469d4be8a163effa601f278d8c
Skye Soss <skye@soss.website>
2026-06-01 14:57:14 -0500

    radicle-ci-ambient: 0.13.0 -> 0.19.0

--- NixOS/pkgs/radicle-ci-ambient.nix
+++ NixOS/pkgs/radicle-ci-ambient.nix
@@ -2,17 +2,20 @@
   lib,
   fetchCrate,
   rustPlatform,
+  nix-update-script,
 }:
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "radicle-ci-ambient";
-  version = "0.13.0";
+  version = "0.19.0";
 
   src = fetchCrate {
     inherit (finalAttrs) pname version;
-    hash = "sha256-7dD7nQMNJ5MpxRGj9dcSe6ZXboteHoW+nkzrWIsCsAY=";
+    hash = "sha256-zTvuzipqQ4/yWnnQd+eaIdVYUruQwvKMEm6yBCSRuJs=";
   };
 
   cargoHash = "sha256-bJ0yE947GawqR1XcQonLnHpTliJ5W9Xng6wEH0dlzjw=";
+
+  passthru.updateScript = nix-update-script { };
 
   meta = {
     description = "Radicle CI adapter for Ambient";
