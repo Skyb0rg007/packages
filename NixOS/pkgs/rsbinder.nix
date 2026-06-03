42770b2b0e50da178a248e804bde2c0d5e7b7f0e
Skye Soss <skye@soss.website>
2026-06-03 14:52:59 -0500

    rsbinder: fetch from GitHub

--- NixOS/pkgs/rsbinder.nix
+++ NixOS/pkgs/rsbinder.nix
@@ -1,48 +1,44 @@
 {
-  fetchCrate,
+  fetchFromGitHub,
   lib,
   rustPlatform,
-  nix-update-script,
 }:
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "rsbinder";
   version = "0.8.0";
 
-  src = fetchCrate {
-    inherit (finalAttrs) pname version;
-    hash = "sha256-o4/6rA0QRMmBxlkssXraNagIvVhBvzO7iCMqviUUEic=";
+  src = fetchFromGitHub {
+    owner = "hiking90";
+    repo = "rsbinder";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-5VF5Hdn3vL6TJ1Rcu4rC59nLoNEyDXP9p69zrZUnx6c=";
   };
+
+  cargoHash = "sha256-3HK7fDmm0cyGf2Hq2OlOc3rk5QpLsosz9xYIUQ1eCY4=";
 
   checkFlags = [
     # These tests require a mounted binderfs
+    "--skip=test_client::"
+    "--skip=test_sm::"
+    "--skip=process_state::"
     "--skip=binderfs::tests::test_add_device"
-    "--skip=process_state::tests::test_concurrent_strong_proxy_case_b_resurrection"
-    "--skip=process_state::tests::test_concurrent_strong_proxy_same_handle_returns_same_arc"
-    "--skip=process_state::tests::test_native_dedup_same_arc"
-    "--skip=process_state::tests::test_native_distinct_arcs_get_distinct_ids"
-    "--skip=process_state::tests::test_native_lookup_does_not_change_counts"
-    "--skip=process_state::tests::test_native_uaf_window_closed"
-    "--skip=process_state::tests::test_process_state"
-    "--skip=process_state::tests::test_process_state_context_object"
-    "--skip=process_state::tests::test_process_state_disable_background_scheduling"
-    "--skip=process_state::tests::test_process_state_start_thread_pool"
-    "--skip=process_state::tests::test_process_state_strong_proxy_for_handle"
-    "--skip=process_state::tests::test_strong_proxy_under_same_thread_dead_binder_no_deadlock"
     "--skip=tests::process_state"
     "--skip=thread_state::tests::test_process_pending_derefs_handles_reentrant_push_from_drop"
   ];
 
-  cargoHash = "sha256-4oSLyZ1g43UqhGtMUBPI+jRcUVhdvwsw/VADS86frt0=";
-
-  passthru.updateScript = nix-update-script { };
-
   meta = {
-    description = "Binder IPC for Linux";
+    description = "CLI tools for Binder IPC on Linux";
     longDescription = ''
-      rsbinder provides crates implemented in pure Rust that make Binder
-      IPC available on both Android and Linux.
+      rsbinder-tools provides two tools:
+
+      rsb_device is a utility for initializing the Linux binder
+      environment and creating binder device files.
+
+      rsb_hub is a comprehensive service manager for Linux that replaces
+      Android's service_manager functionality.
     '';
     homepage = "https://github.com/hiking90/rsbinder";
     license = lib.licenses.asl20;
+    platforms = lib.platforms.linux;
   };
 })
