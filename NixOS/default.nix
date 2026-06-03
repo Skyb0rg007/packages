277dbdb410895819c05f129bf506f954338ba1aa
Skye Soss <skye@soss.website>
2026-06-03 17:12:23 -0500

    Remove packages that don't build from default.nix

--- NixOS/default.nix
+++ NixOS/default.nix
@@ -10,20 +10,15 @@ rec {
   mxc = pkgs.callPackage ./pkgs/mxc.nix { };
   cascade = pkgs.callPackage ./pkgs/cascade.nix { };
   sbuild = pkgs.callPackage ./pkgs/sbuild.nix { };
-  keylime = pkgs.callPackage ./pkgs/keylime.nix { };
-  rust-keylime = pkgs.callPackage ./pkgs/rust-keylime.nix { };
   rsbinder-tools = pkgs.callPackage ./pkgs/rsbinder-tools.nix { };
-  brush-HEAD = pkgs.callPackage ./pkgs/brush.nix { };
   run0-wrappers = pkgs.callPackage ./pkgs/run0-wrappers.nix { };
   masque-go = pkgs.callPackage ./pkgs/masque-go.nix { };
   image-builder = pkgs.callPackage ./pkgs/image-builder.nix { };
 
-  # Radicle
-  radicle-ci-ambient = pkgs.callPackage ./pkgs/radicle-ci-ambient.nix { };
-
   # Lars Wirzenius's Packages
   ambient-build-vm = pkgs.callPackage ./pkgs/ambient-build-vm.nix { };
   ambient-ci = pkgs.callPackage ./pkgs/ambient-ci.nix { };
+  radicle-ci-ambient = pkgs.callPackage ./pkgs/radicle-ci-ambient.nix { };
   subplot = pkgs.callPackage ./pkgs/subplot.nix { };
   vmdb2 = pkgs.callPackage ./pkgs/vmdb2.nix { };
 
@@ -31,7 +26,7 @@ rec {
   ascsaver = pkgs.callPackage ./pkgs/ascsaver.nix { };
   mlkit = pkgs.callPackage ./pkgs/mlkit.nix { };
   barry = pkgs.callPackage ./pkgs/barry.nix { inherit mlkit; };
-  smltojs = pkgs.callPackage ./pkgs/smltojs.nix { };
+  smltojs = pkgs.callPackage ./pkgs/smltojs.nix { inherit mlkit; };
   bencher = pkgs.callPackage ./pkgs/bencher.nix { };
   browsertrix-crawler = pkgs.callPackage ./pkgs/browsertrix-crawler.nix { };
   cdash = pkgs.callPackage ./pkgs/cdash.nix { };
