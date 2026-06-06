89152184c98d1f64fd66c2a88e7719bdb5f06603
Skye Soss <skye@soss.website>
2026-06-05 19:20:43 -0500

    debian-archive-keyring: cleanup

--- NixOS/pkgs/by-name/debian-archive-keyring/package.nix
+++ NixOS/pkgs/by-name/debian-archive-keyring/package.nix
@@ -3,8 +3,10 @@
   fetchFromGitLab,
   stdenvNoCC,
   nix-update-script,
+  jetring,
+  gnupg,
 }:
-stdenvNoCC.mkDerivation {
+stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "debian-archive-keyring";
   version = "2025.1";
 
@@ -12,16 +14,29 @@ stdenvNoCC.mkDerivation {
     domain = "salsa.debian.org";
     owner = "release-team";
     repo = "debian-archive-keyring";
-    rev = "2025.1";
+    tag = finalAttrs.version;
     hash = "sha256-NaVbca1mx0j4hfSncX8hh8PbtB92yGeZUUdp4Nx9JoY=";
   };
 
-  dontBuild = true;
+  makeFlags = [ "PREFIX=$(out)" ];
+
+  nativeBuildInputs = [
+    jetring
+    gnupg
+  ];
 
   installPhase = ''
     runHook preInstall
 
-    install -Dm644 -t $out/etc/apt/trusted.gpg.d apt-trusted-asc/*.asc
+    install -d $out/share/keyrings/
+    cp trusted.pgp/debian-archive-*.pgp $out/share/keyrings/
+    cp -a trusted.pgp/debian-archive-*.gpg $out/share/keyrings/
+    cp keyrings/debian-archive-keyring.pgp $out/share/keyrings/
+    cp -a keyrings/debian-archive-keyring.gpg $out/share/keyrings/
+    cp keyrings/debian-archive-removed-keys.pgp $out/share/keyrings/
+    cp -a keyrings/debian-archive-removed-keys.gpg $out/share/keyrings/
+
+    install -D -m 644 -t $out/etc/apt/trusted.gpg.d apt-trusted-asc/*.asc
 
     runHook postInstall
   '';
@@ -31,7 +46,10 @@ stdenvNoCC.mkDerivation {
   meta = {
     description = "GnuPG archive keys of the Debian archive";
     homepage = "https://salsa.debian.org/release-team/debian-archive-keyring";
-    license = lib.licenses.gpl2Plus;
+    license = [
+      lib.licenses.gpl2Plus
+      lib.licenses.publicDomain
+    ];
     platforms = lib.platforms.all;
   };
-}
+})
