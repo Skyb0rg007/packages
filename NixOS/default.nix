0afe091550e44dac49ef5ad312c12ea7e0f5c3b2
Skye Soss <skye@soss.website>
2026-06-09 14:42:51 -0500

    tcl-syslog: init at 2.0.2

--- NixOS/default.nix
+++ NixOS/default.nix
@@ -8,12 +8,16 @@ let
   nixosTests = import ./tests/all-tests.nix {
     inherit pkgs nixosModules packages;
   };
+  # XXX: Merge pkgs and packages so `pkgs.python3Packages` is merged
   callPackage = lib.callPackageWith (pkgs // packages // { inherit nixosTests; });
   callPython3PackageWith =
     name:
     lib.callPackageWith (
       pkgs // packages // pkgs.${name} // packages.${name} // { inherit nixosTests; }
     );
+  callTcl9Package = lib.callPackageWith (
+    pkgs // packages // pkgs.tcl9Packages // packages.tcl9Packages // { inherit nixosTests; }
+  );
 
   # These override nixpkgs
   # XXX: Give mlkit, etc. different names from nixpkgs
@@ -41,6 +45,10 @@ let
       python315Packages = lib.filesystem.packagesFromDirectoryRecursive {
         callPackage = callPython3PackageWith "python315Packages";
         directory = ./pkgs/python-modules;
+      };
+      tcl9Packages = lib.filesystem.packagesFromDirectoryRecursive {
+        callPackage = callTcl9Package;
+        directory = ./pkgs/tcl-modules;
       };
     };
 in
