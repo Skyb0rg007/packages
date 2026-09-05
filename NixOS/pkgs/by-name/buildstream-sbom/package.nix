1ee0fa189e3b0a1f3acce2a608b52ebc46333373
Skye Soss <skye@soss.website>
2026-09-05 09:44:35 -0500

    buildstream-sbom: init at 1.1

---
+++ NixOS/pkgs/by-name/buildstream-sbom/package.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  fetchFromGitLab,
+  python3Packages,
+}:
+python3Packages.buildPythonApplication (finalAttrs: {
+  pname = "buildstream-sbom";
+  version = "1.1";
+  pyproject = true;
+  strictDeps = true;
+  __structuredAttributes = true;
+
+  src = fetchFromGitLab {
+    owner = "BuildStream";
+    repo = "buildstream-sbom";
+    tag = finalAttrs.version;
+    hash = "sha256-YuqKWQX4UnTFcPpYTa3tlgkeTYc9khfZe1PAW84yv5I=";
+  };
+
+  build-system = [
+    python3Packages.setuptools-scm
+  ];
+
+  dependencies = [
+    python3Packages.pyyaml
+  ];
+
+  meta = {
+    homepage = "https://gitlab.com/BuildStream/buildstream-sbom/";
+    description = "Produce SBOMs describing BuildStream elements and dependencies";
+    license = lib.licenses.asl20;
+    changelog = "https://gitlab.com/BuildStream/buildstream-sbom/-/blob/${finalAttrs.version}/CHANGELOG.md";
+  };
+})
